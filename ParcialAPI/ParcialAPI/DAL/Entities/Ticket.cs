1b2be97a2542b2ead3eba615c0e5ca431f33197a
MIGUEL ÁNGEL RÍOS OCHOA <miguelrios312671@correo.itm.edu.co>
2023-05-21 14:58:15 -0500

    New project Parcial_API and Entity Ticket created.

---
+++ ParcialAPI/ParcialAPI/DAL/Entities/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParcialAPI.DAL.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [Display(Name ="Fecha uso")]
+        public DateTime? UseDate { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")]
+        public Boolean IsUsed { get; set; }
+
+        [Display(Name = "Entrada")]
+        [MaxLength(10, ErrorMessage ="El campo {0} debe tener máximo {1} caracteres.")]
+        public string? EntranceGate { get; set; }
+    }
+}
