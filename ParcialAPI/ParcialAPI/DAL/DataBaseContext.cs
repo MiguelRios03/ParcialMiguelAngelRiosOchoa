c677bb7fe8c710905507505904f9823fdbed1391
MIGUEL ÁNGEL RÍOS OCHOA <miguelrios312671@correo.itm.edu.co>
2023-05-21 15:31:38 -0500

    New DataBaseContext.cs, migration and database ConcertDB_MiguelAngelRiosOchoa created

---
+++ ParcialAPI/ParcialAPI/DAL/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParcialAPI.DAL.Entities;
+
+namespace ParcialAPI.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+    }
+}
