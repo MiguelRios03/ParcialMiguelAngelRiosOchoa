4522d0c0de06ac1f0c35b28730fba23c1ff32cf7
MIGUEL ÁNGEL RÍOS OCHOA <miguelrios312671@correo.itm.edu.co>
2023-05-22 13:21:00 -0500

    Get consumed by API succesfully

--- ParcialAPI/ParcialAPI/Controllers/TicketController.cs
+++ ParcialAPI/ParcialAPI/Controllers/TicketController.cs
@@ -9,7 +9,7 @@ namespace ParcialAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TicketsController : ControllerBase
+    public class TicketController : ControllerBase
     {
         private static readonly string[] Entrances = new[]
         {
@@ -18,13 +18,24 @@ namespace ParcialAPI.Controllers
 
         private readonly DataBaseContext _context;
 
-        public TicketsController(DataBaseContext context)
+        public TicketController(DataBaseContext context)
         {
             _context = context;
         }
 
         [HttpGet, ActionName("Get")]
         [Route("Get")]
+        public async Task<ActionResult<IEnumerable<Ticket>>> GetTickets()
+        {
+            var tickets = await _context.Tickets.ToListAsync();
+
+            if (tickets == null) return NotFound();
+
+            return tickets;
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("Get/id")]
         public async Task<ActionResult<Ticket>> GetTicketById(Guid? id)
         {
             var ticket = await _context.Tickets.FirstOrDefaultAsync(c => c.ID == id);
@@ -35,23 +46,29 @@ namespace ParcialAPI.Controllers
 
             await EditTicket(ticket.ID, ticket);
 
-            return Ok(String.Format("Boleta válida, puede ingresar al concierto"));
+            return Ok(String.Format("Boleta valida, puede ingresar al concierto"));
         }
 
-        [HttpPut, ActionName("Edit")]
+        [HttpPost, ActionName("Edit")]
         [Route("Edit")]
 
         public async Task<ActionResult<Ticket>> EditTicket(Guid? id, Ticket ticket)
         {
+            try
+            {
+                ticket.UseDate = DateTime.Now;
+                ticket.IsUsed = true;
+                ticket.EntranceGate = Entrances[Random.Shared.Next(Entrances.Length)];
 
-            ticket.UseDate = DateTime.Now;
-            ticket.IsUsed = true;
-            ticket.EntranceGate = Entrances[Random.Shared.Next(Entrances.Length)];
+                _context.Tickets.Update(ticket);
+                await _context.SaveChangesAsync();
 
-            _context.Tickets.Update(ticket);
-            await _context.SaveChangesAsync();
+            }catch(Exception ex)
+            {
+                return Conflict(ex.Message);
+            }
 
-            return Ok(String.Format("Boleta válida, puede ingresar al concierto"));
+            return Ok(ticket);
         } 
         
     }
--- ParcialAPI/WebPagesAPI/WebPages/Controllers/TicketController.cs
+++ ParcialAPI/WebPagesAPI/WebPages/Controllers/TicketController.cs
@@ -8,16 +8,72 @@ namespace WebPages.Controllers
     public class TicketController : Controller
     {
         private readonly IHttpClientFactory _httpClient;
+        private readonly IConfiguration _configuration;
 
-        public TicketController(IHttpClientFactory httpClient)
+        public TicketController(IHttpClientFactory httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
         }
-        public async Task<IActionResult> Edit(Guid id)
+
+        public async Task<IActionResult> Index()
         {
-            var url = String.Format("https://localhost:7258/api/Tickets/Edit/{0}", id);
-            await _httpClient.CreateClient().PutAsJsonAsync(url, id);
-            return View("Index");
+            try
+            {
+                //var url = "https://localhost:7258/api/Ticket/Get/";
+                //var json = await _httpClient.CreateClient().GetStringAsync(url);
+                //List<Ticket> tickets = JsonConvert.DeserializeObject<List<Ticket>>(json);
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return View("Error", ex);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTicketById()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetTicketById(Guid? id)
+        {
+            try
+            {
+                var url = String.Format("https://localhost:7258/api/Ticket/Get/{0}", id);
+                var json = await _httpClient.CreateClient().GetStringAsync(url);
+                return RedirectToAction("Edit", JsonConvert.DeserializeObject<Ticket>(json));
+            }
+            catch(Exception ex)
+            {
+                return View("Error", ex);
+            }
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Ticket ticket)
+        {
+            try
+            {
+                var url = String.Format("https://localhost:7258/api/Ticket/Edit/{0}", id);
+            await _httpClient.CreateClient().PutAsJsonAsync(url, ticket);
+            return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View("Error", ex);
+            }
         }
     }
 }
